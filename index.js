1ba2ce40d19810343538b45c89a263baafdb5def
Nancy Eckenthal <nancy.eckenthal@gmail.com>
2017-09-07 03:05:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,10 +11,10 @@ function logWhisper(string) {
     console.log(string.toLowerCase())
 }
 function sayHiToGrandma(string) {
-    if string === string.toLowerCase(); {
+    if (string === string.toLowerCase()); {
         return "I can't hear you!"
     }
-    else if string === string.toUpperCase(); {
+    else if (string === string.toUpperCase()); {
         return "YES INDEED!"
     }
 }
