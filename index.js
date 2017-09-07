8a0621aa01531bc0e269e6788db462a0377f85da
Nancy Eckenthal <nancy.eckenthal@gmail.com>
2017-09-07 03:11:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,6 +14,9 @@ function sayHiToGrandma(string) {
     if (string.toLowerCase() === string) {
         return "I can't hear you!";
     } else if (string.toUpperCase() === string) {
-        return "YES INDEED!"
+        return "YES INDEED!";
+    } else if (string === "I love you, Grandma.") {
+        return "I love you, too."
     }
 }
+
